6085468555a847789d57159975c026767d5ec03e
mihir-robotics <mihir.kulkarni.808@gmail.com>
2023-12-18 23:04:20 +0530

    should these mean something?

--- examples/LED_Relay_Buzzer.c
+++ examples/LED_Relay_Buzzer.c
@@ -3,13 +3,18 @@
  *  Created on: 10-Aug-2021
  */
 
+// C8051F340 library
 #include "c8051F340.h"
+// Set LED on Port 4
 #define LED P4
 
-sbit KEY1 = P1^0;
-sbit KEY2 = P1^1;
-sbit RELAY = P1^4;
-sbit BUZZER = P3^3;
+// Define the keys
+sbit KEY1 = P1^0;	// Port 1 Pin 0
+sbit KEY2 = P1^1;	// Port 1 Pin 1
+
+// Define the relay and buzzer connections
+sbit RELAY = P1^4;	// Port 1 Pin 4
+sbit BUZZER = P3^3; // Port 3 Pin 3
 
 void main(){
 
